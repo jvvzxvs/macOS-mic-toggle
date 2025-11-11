88a03b27e19b78be760bbadb847ac8d8e3fe86a3
jvvzxvs <nikita021282@gmail.com>
2025-11-11 23:31:37 +0300

    Initial Commit

---
+++ macOS mic toggle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  macOS mic toggle
+//
+//  Created by никита никита on 11.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ macOS mic toggle/macOS_mic_toggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  macOS_mic_toggleApp.swift
+//  macOS mic toggle
+//
+//  Created by никита никита on 11.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct macOS_mic_toggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
